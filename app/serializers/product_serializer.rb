11f5444b28d4ebaaf388034a6cffc4422ad453ea
Veronica Bustabad <vbustabad@gmail.com>
2018-10-05 20:48:04 +0000

    Done.

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -5,9 +5,9 @@ class ProductSerializer
 
     serialized_product += '"id": ' + product.id.to_s + ', '
     serialized_product += '"name": "' + product.name + '", '
-    serialized_product += '"price": "' + product.price.to_s + '", '
-    serialized_product += '"inventory": "' + product.inventory.to_s + '", '
-    serialized_product += '"description": "' + product.description + '", '
+    serialized_product += '"price": ' + product.price.to_s + ', '
+    serialized_product += '"inventory": ' + product.inventory.to_s + ', '
+    serialized_product += '"description": "' + product.description + '"'
 
     serialized_product += '}'
   end
