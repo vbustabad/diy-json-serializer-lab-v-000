8709ec875e11f237783840132a8b79ebfd7b5650
Veronica Bustabad <vbustabad@gmail.com>
2018-10-05 20:40:04 +0000

    automatically backed up by learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -6,7 +6,7 @@ class ProductSerializer
     serialized_product += '"id": ' + product.id.to_s + ', '
     serialized_product += '"name": "' + product.name + '", '
     serialized_product += '"price": "' + product.price.to_s + '", '
-    serialized_product += '"inventory": "' + product.inventory + '", '
+    serialized_product += '"inventory": "' + product.inventory.to_s + '", '
     serialized_product += '"description": "' + product.description + '"'
 
     serialized_product += '}'
